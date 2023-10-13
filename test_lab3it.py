df2a2390715e352010b3c6408acadbb76a558c99
Nazarii <nazarshkwarok@gmail.com>
2023-10-14 00:27:45 +0300

    first commit7

--- test_lab3it.py
+++ test_lab3it.py
@@ -6,7 +6,7 @@ def test_addition():
    v = [5, 6]
    result = lab3it_function.add(2, 3)
    assert result in v
-#12312322222222225465454
+
 def test_negative_numbers():
    v = [5, 0]
    result = lab3it_function.add(-5, 10)
